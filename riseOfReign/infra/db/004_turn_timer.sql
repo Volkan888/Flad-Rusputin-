1482fc434513e6f95dd792e2df347f9f748da954
Volkan888 <kivoku@web.de>
2026-09-02 06:04:26 +0200

    2026-09-02 06:07 CEST | milestone-26i | align database timer presets to voted five-option system

--- riseOfReign/infra/db/004_turn_timer.sql
+++ riseOfReign/infra/db/004_turn_timer.sql
@@ -1,12 +1,23 @@
 ALTER TABLE matches
-    ADD COLUMN IF NOT EXISTS timer_preset text NOT NULL DEFAULT 'live_standard';
+    ADD COLUMN IF NOT EXISTS timer_preset text NOT NULL DEFAULT 'turn_5m';
 
 ALTER TABLE matches
     DROP CONSTRAINT IF EXISTS ck_matches_timer_preset;
 
+UPDATE matches
+SET timer_preset = CASE timer_preset
+    WHEN 'live_quick' THEN 'turn_5m'
+    WHEN 'live_standard' THEN 'turn_5m'
+    WHEN 'live_extended' THEN 'turn_10m'
+    WHEN 'async_12h' THEN 'turn_24h'
+    WHEN 'async_24h' THEN 'turn_24h'
+    WHEN 'async_48h' THEN 'turn_24h'
+    ELSE timer_preset
+END;
+
 ALTER TABLE matches
     ADD CONSTRAINT ck_matches_timer_preset
-    CHECK (timer_preset IN ('live_quick','live_standard','live_extended','async_12h','async_24h','async_48h'));
+    CHECK (timer_preset IN ('turn_2m','turn_5m','turn_10m','turn_1h','turn_24h'));
 
 ALTER TABLE match_players
     ADD COLUMN IF NOT EXISTS timeout_count integer NOT NULL DEFAULT 0;
